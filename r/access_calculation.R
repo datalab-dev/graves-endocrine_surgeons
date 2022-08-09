dd7b0f5db4a961f9923a31f9a03b1a1d1919f91b
Michele Tobias <mmtobias@ucdavis.edu>
2022-08-08 17:11:08 -0700

    create a function

--- r/access_calculation.R
+++ r/access_calculation.R
@@ -11,6 +11,61 @@ library(sf)
 library(geojsonsf)
 
 
+# Custom Functions
+calc_access<-function(tracts, isochrones, crs=5070 ){
+  
+    # Process the Isochrones
+  isochrones<-st_cast(isochrones, to="POLYGON" ) #cast the linestring to a polygon
+  isochrones<-st_union(isochrones) #put all the polygons into one layer
+  
+  # Coordinate Reference System
+  # Default Coordinate Reference System = EPSG 5070 is USA Contiguous Albers Equal Area Conic
+  # transform the data into the same CRS
+  tracts<-st_transform(tracts, crs)
+  isochrones<-st_transform(isochrones, crs)
+  
+  #calculate the area of each tract
+  tracts$tract_area_meters<-st_area(tracts)
+  
+  #intersect the tracts with the isochrone
+  #not sure which tool is the right one 
+  #   - SF's st_intersect and st_union aren't right
+  #   - terra's union isn't right
+  #   - sfhelpers st_or() didn't work either
+  # 2 step process with SF? st_difference + st_intersection - https://stackoverflow.com/questions/54710574/how-to-do-a-full-union-with-the-r-package-sf 
+  
+  inside<-st_intersection(tracts, isochrones)
+  outside<-st_difference(tracts, isochrones)
+  
+  #calculate the area of each piece
+  inside$inside_area_meters<-st_area(inside)
+  outside$outside_area_meters<-st_area(outside)
+  
+  #calclulate the percent area of the new/old areas
+  inside$inside_percent<-as.numeric(inside$inside_area_meters)/as.numeric(inside$tract_area_meters)
+  outside$outside_percent<-as.numeric(outside$outside_area_meters)/as.numeric(outside$tract_area_meters)
+  
+  #how many people are in each census tract piece?
+  inside$inside_pop<-inside$inside_percent*inside$estimate
+  outside$outside_pop<-outside$outside_percent*outside$estimate
+  
+  #how many people are inside vs. outside the isochrone?
+  has_access<-sum(inside$inside_pop)
+  no_access<-sum(outside$outside_pop)
+  
+  access_results<-as.data.frame(
+    matrix(
+      data=c(has_access, no_access), 
+      nrow = 1, 
+      ncol = 2, 
+      byrow= TRUE,
+      dimnames=list(c(NULL), c("has_access", "no_access")) #row then col names
+      ), 
+    )
+  
+  return(access_results)
+}
+
 # Load Data
 tracts<- readRDS("./data/tract_population.rds")
 
@@ -21,52 +76,20 @@ tracts<- readRDS("./data/tract_population.rds")
 
 #isochrones<-geojson_sf(geojson="D:\\Graves_Endocrine_Surgery\\data\\isochrones\\isochrone_ucdhealth_100minutes.json")
 
-isochrones<- readRDS("C:\\Users\\mmtobias\\Downloads\\isochrones_90_min.rds")
+#isochrones<- readRDS("C:\\Users\\mmtobias\\Downloads\\isochrones_90_min.rds")
 
 
-# process the data
-isochrones<-st_cast(isochrones, to="POLYGON" ) #cast the linestring to a polygon
+# Get the list of isochrone data files
+iso_list<-list.files("./data", pattern="^isochrones", full.names = TRUE)
 
-isochrones<-st_union(isochrones) #put all the polygons into one layer
-
-# Coordinate Reference System
-# EPSG 5070 is USA Contiguous Albers Equal Area Conic
-
-tracts<-st_transform(tracts, crs=5070)
-isochrones<-st_transform(isochrones, crs=5070)
-
-# Analysis ----------------------------------------------------------------
-
-#calculate the area of each tract
-tracts$tract_area_meters<-st_area(tracts)
-
-#intersect the tracts with the isochrone
-#not sure which tool is the right one 
-#   - SF's st_intersect and st_union aren't right
-#   - terra's union isn't right
-#   - sfhelpers st_or() didn't work either
-# 2 step process with SF? st_difference + st_intersection - https://stackoverflow.com/questions/54710574/how-to-do-a-full-union-with-the-r-package-sf 
-
-inside<-st_intersection(tracts, isochrones)
-outside<-st_difference(tracts, isochrones)
-
-#calculate the area of each piece
-inside$inside_area_meters<-st_area(inside)
-outside$outside_area_meters<-st_area(outside)
-
-#calclulate the percent area of the new/old areas
-inside$inside_percent<-as.numeric(inside$inside_area_meters)/as.numeric(inside$tract_area_meters)
-outside$outside_percent<-as.numeric(outside$outside_area_meters)/as.numeric(outside$tract_area_meters)
-
-#how many people are in each census tract piece?
-inside$inside_pop<-inside$inside_percent*inside$estimate
-outside$outside_pop<-outside$outside_percent*outside$estimate
-
-#how many people are inside vs. outside the isochrone?
-has_access<-sum(inside$inside_pop)
-no_access<-sum(outside$outside_pop)
-
-
+# Run the Calculation
+for (i in iso_list){
+  print(i)
+  isochrone<-readRDS(i)
+  print("RDS read")
+  access_numbers<-calc_access(tracts, isochrone)
+  print(access_numbers)
+}
 
 
 
