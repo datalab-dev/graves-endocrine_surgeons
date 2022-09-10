8561f04907a3e9aaecc386a725d54947e6d0c22d
Michele Tobias <mmtobias@ucdavis.edu>
2022-09-09 17:07:18 -0700

    refactor with sf functions instead of QGIS

--- r/access_calculation.R
+++ r/access_calculation.R
@@ -12,23 +12,43 @@ library(qgisprocess)
 
 
 # Custom Functions
+
+# fix invalid polygons
+fix_invalid<-function(polygons){
+  
+  #test to see if any polygons are invalid
+  valid_test<-st_is_valid(polygons)
+  
+  #   If any invalid polygons are found, fix them; report back in either case
+  if (FALSE %in% valid_test){
+    print("One or more polygons was invalid. Using st_make_valid() to fix errors.")
+    polygons<-st_make_valid(polygons)
+  }else{
+    print("No invalid polygons detected.")
+  }
+  
+  return(polygons)
+}
+
+
+
 calc_access<-function(tracts, isochrones, crs=5070, tempdirectory="./data/temporary" ){
+  
+  #make a temporary directory to hold the files, if it doesn't already exist
+  ifelse(
+    !dir.exists(file.path(tempdirectory)), 
+    dir.create(file.path(tempdirectory)), 
+    FALSE) 
   
   #remove the files in the temporary directory
   do.call(file.remove, list(list.files(tempdirectory, full.names = TRUE)))
   
 
   # Check for invalid geometries (repeated vertexes)
+  isochrones<-fix_invalid(isochrones)
+  tracts<-fix_invalid(tracts)
   
-  #   Test for valid polygons
-  valid_test<-st_is_valid(isochrones)
-  #   If any invalid polygons are found, fix them; report back in either case
-  if (FALSE %in% valid_test){
-    print("One or more polygons was invalid. Using st_make_valid() to fix errors.")
-    isochrones<-st_make_valid(isochrones)
-  }else{
-    print("No invalid polygons detected.")
-  }
+
   
   
   # Coordinate Reference System
@@ -42,23 +62,19 @@ calc_access<-function(tracts, isochrones, crs=5070, tempdirectory="./data/tempor
   # Put all the polygons into one layer - QGIS' dissolve does this
   #   sf's st_union() has odd, nested results that aren't helpful
   
-  #make a temporary directory to hold the files, if it doesn't already exist
-  ifelse(
-    !dir.exists(file.path(tempdirectory)), 
-    dir.create(file.path(tempdirectory)), 
-    FALSE) 
-  
-  #remove the ID column because it's a list and causing problems
-  isochrones<-isochrones[,-9]
-  
-  #write the isochrones files to the temporary file
-  st_write(isochrones, paste0(tempdirectory,"/isochrones.gpkg"), driver= "GPKG")
 
   
-  #define the input and output files
-  dissolved_file<-file.path(paste0(tempdirectory,"/dissolve_output.gpkg"))
-  isochrone_file<-file.path(paste0(tempdirectory,"/isochrones.gpkg"))
-  dissolved_multi<-file.path(paste0(tempdirectory,"/dissolve_multi.gpkg"))
+  #reduce the number of unnecessary attributes
+  isochrones<-isochrones[,8]
+  
+  # #write the isochrones files to the temporary file
+  # st_write(isochrones, paste0(tempdirectory,"/isochrones.gpkg"), driver= "GPKG")
+  # 
+  # 
+  # #define the input and output files
+  # dissolved_file<-file.path(paste0(tempdirectory,"/dissolve_output.gpkg"))
+  # isochrone_file<-file.path(paste0(tempdirectory,"/isochrones.gpkg"))
+  # dissolved_multi<-file.path(paste0(tempdirectory,"/dissolve_multi.gpkg"))
 
   # Dissolve the isochrones into one multipolygon, then explode it into it's component polygons
   #this is making too big of polygons on the east coast
@@ -75,86 +91,92 @@ calc_access<-function(tracts, isochrones, crs=5070, tempdirectory="./data/tempor
   #   OUTPUT = dissolved_multi
   # )
   
+  #dissolve the isochrones into one big polygon - becomes a "Large sfc_MULTIPOLYGON"
+  isochrones_dissolved<-st_union(isochrones)
   
   #isochrones_dissolved<-st_read(dissolved_multi)
-  isochrones_dissolved<-isochrones
-  isochrones_dissolved$inside<-1
-  isochrones_dissolved_minimal<-isochrones_dissolved[,10]
+  # isochrones_dissolved<-isochrones
+  # isochrones_dissolved$inside<-1
+  # isochrones_dissolved_minimal<-isochrones_dissolved[,10]
   
 
-  
   #calculate the area of each tract
   tracts$tract_area_meters<-st_area(tracts)
   
+  #union the tracts and isochrones
+  inside<-st_intersection(tracts, isochrones_dissolved)
+  outside<-st_difference(tracts, isochrones_dissolved)
+  
   #write the tract to a file
-  st_write(tracts, paste0(tempdirectory,"/tracts.gpkg"), driver= "GPKG")  
+  #st_write(tracts, paste0(tempdirectory,"/tracts.gpkg"), driver= "GPKG")  
   
     
-  # Union (QGIS) the tracts with the isochrone
-  tracts_file<-file.path(paste0(tempdirectory,"/tracts.gpkg"))
-  union_input<-file.path(paste0(tempdirectory,"/tracts.gpkg"))
-  
-  
-  #seed the union file with the tracts to start
-  #st_write(tracts, paste0(tempdirectory,"/union.gpkg"), driver= "GPKG")
-  
-  for (i in 1:length(isochrones_dissolved_minimal$geom)){
-    print(i)
-    
-    (dissolved_file_i<-file.path(paste0(tempdirectory,"/dissolved_", i,".gpkg")))
-    (union_output<-file.path(paste0(tempdirectory,"/union_", i, ".gpkg")))
-    
-    #write the i-th isochrone to the disc
-    st_write(isochrones_dissolved_minimal[i,], dissolved_file_i, driver="GPKG")
-    
-    #qgis_show_help("native:union")
-    
-    qgis_run_algorithm(
-      "native:union",
-      INPUT = union_input,
-      OVERLAY = dissolved_file_i,
-      FIELD = "[]",
-      OUTPUT = union_output
-    )
-    
-    union_input<-union_output
-    
-    #remove unnecessary files
-    file.remove(dissolved_file_i)
-    
-    ifelse(
-      file.exists(file.path(paste0(tempdirectory,"/union_", i-1, ".gpkg"))), 
-      file.remove(file.path(paste0(tempdirectory,"/union_", i-1, ".gpkg"))), 
-      FALSE) 
+  # # Union (QGIS) the tracts with the isochrone
+  # tracts_file<-file.path(paste0(tempdirectory,"/tracts.gpkg"))
+  # union_input<-file.path(paste0(tempdirectory,"/tracts.gpkg"))
+  # 
+  # 
+  # #seed the union file with the tracts to start
+  # #st_write(tracts, paste0(tempdirectory,"/union.gpkg"), driver= "GPKG")
+  # 
+  # for (i in 1:length(isochrones_dissolved_minimal$geom)){
+  #   print(i)
+  #   
+  #   (dissolved_file_i<-file.path(paste0(tempdirectory,"/dissolved_", i,".gpkg")))
+  #   (union_output<-file.path(paste0(tempdirectory,"/union_", i, ".gpkg")))
+  #   
+  #   #write the i-th isochrone to the disc
+  #   st_write(isochrones_dissolved_minimal[i,], dissolved_file_i, driver="GPKG")
+  #   
+  #   #qgis_show_help("native:union")
+  #   
+  #   qgis_run_algorithm(
+  #     "native:union",
+  #     INPUT = union_input,
+  #     OVERLAY = dissolved_file_i,
+  #     FIELD = "[]",
+  #     OUTPUT = union_output
+  #   )
+  #   
+  #   union_input<-union_output
+  #   
+  #   #remove unnecessary files
+  #   file.remove(dissolved_file_i)
+  #   
+  #   ifelse(
+  #     file.exists(file.path(paste0(tempdirectory,"/union_", i-1, ".gpkg"))), 
+  #     file.remove(file.path(paste0(tempdirectory,"/union_", i-1, ".gpkg"))), 
+  #     FALSE) 
+  # 
+  #   ifelse(
+  #     file.exists(file.path(paste0(tempdirectory,"/union_", i-1, ".gpkg"))), 
+  #     file.remove(file.path(paste0(tempdirectory,"/dissolved_", i-1,".gpkg"))), 
+  #     FALSE)     
+  # 
+  # }
 
-    ifelse(
-      file.exists(file.path(paste0(tempdirectory,"/union_", i-1, ".gpkg"))), 
-      file.remove(file.path(paste0(tempdirectory,"/dissolved_", i-1,".gpkg"))), 
-      FALSE)     
-
-  }
-
-  
-  everything<-st_read(union_output)
-  
-  inside_cols<-as.data.frame(everything)
-  inside_cols<-inside_cols[,grep("inside", names(everything))]
-  inside_cols<-rowSums(inside_cols, na.rm = TRUE)
-  
-  everything$inside_iso<-inside_cols
-  
-  #calculate the area of each piece
-  #   Inside: fid_2 != NULL
-  #   Outside: fid_2 = NULL
-  #inside$inside_area_meters<-st_area(inside)
-  #outside$outside_area_meters<-st_area(outside)
-  
-  
-  # inside<-subset(everything, is.na(fid_2)==FALSE )
-  # outside<-subset(everything, is.na(fid_2)==TRUE )
-
-  inside<-subset(everything, inside_iso==1)
-  outside<-subset(everything, inside_iso==0)
+  # 
+  # everything<-st_read(union_output)
+  # everything<-fix_invalid(everything)
+  # 
+  # inside_cols<-as.data.frame(everything)
+  # inside_cols<-inside_cols[,grep("inside", names(everything))]
+  # inside_cols<-rowSums(inside_cols, na.rm = TRUE)
+  # 
+  # everything$inside_iso<-inside_cols
+  # 
+  # #calculate the area of each piece
+  # #   Inside: fid_2 != NULL
+  # #   Outside: fid_2 = NULL
+  # #inside$inside_area_meters<-st_area(inside)
+  # #outside$outside_area_meters<-st_area(outside)
+  # 
+  # 
+  # # inside<-subset(everything, is.na(fid_2)==FALSE )
+  # # outside<-subset(everything, is.na(fid_2)==TRUE )
+  # 
+  # inside<-subset(everything, inside_iso==1)
+  # outside<-subset(everything, inside_iso==0)
   
   inside$inside_area_meters<-st_area(inside)
   outside$outside_area_meters<-st_area(outside)
@@ -181,10 +203,11 @@ calc_access<-function(tracts, isochrones, crs=5070, tempdirectory="./data/tempor
       ), 
     )
   
-  plot(inside$geom)
+  plot(inside$geometry)
 
-  return(inside)
-  return(outside)
+  st_write(inside, file.path(paste0(tempdirectory,"/inside_tracts.gpkg")), driver="GPKG")
+  st_write(outside, file.path(paste0(tempdirectory,"/outside_tracts.gpkg")), driver="GPKG")
+
   return(access_results)
 }
 
@@ -194,7 +217,9 @@ tracts<- readRDS("./data/tract_population.rds")
 # Get the list of isochrone data files
 iso_list<-list.files("./data", pattern="^isochrones", full.names = TRUE)
 
-access_table<-as.data.frame(matrix(nrow=0, ncol=2))
+access_table<-as.data.frame(matrix(nrow=0, ncol=3))
+
+start_time<-Sys.time()
 
 # Run the Calculation
 for (i in iso_list){
@@ -202,9 +227,13 @@ for (i in iso_list){
   isochrone<-readRDS(i)
   print("RDS read")
   access_numbers<-calc_access(tracts, isochrone)
+  access_numbers$isochrone<-i
   access_table<-rbind(access_table, access_numbers)
   print(access_table)
 }
+
+finish_time<-Sys.time()
+(time_elapsed<-finish_time-start_time)
 
 
 
