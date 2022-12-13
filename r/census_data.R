4fee84b3da597d3e36bf68866f5aba8f32757a16
Michele Tobias <mmtobias@ucdavis.edu>
2022-12-12 16:27:55 -0800

    add race api call, add isochrone data

--- r/census_data.R
+++ r/census_data.R
@@ -6,11 +6,18 @@
 # libraries
 #install.packages("tidycensus")
 library(tidycensus)
+library(reshape)
+library(sf)
 
 # working directory
 setwd("D:\\Graves_Endocrine_Surgery\\data")
 
 # load data
+
+# get the list of isochrone data
+isochrone_files<-list.files(".\\isochrones_tracts")
+
+inside_60<-read_sf(".\\isochrones_tracts\\inside_tracts_60.gpkg")
 
 # load the API key from a local file
 con<-file("D:\\Graves_Endocrine_Surgery\\census_API_key.txt")
@@ -19,7 +26,8 @@ close(con)
 
 
 
-# Analysis ----------------------------------------------------------------
+
+# Searching Variables -----------------------------------------------------
 
 # search for variables
 vars_pl_2020<-load_variables(2020, "pl")
@@ -35,45 +43,85 @@ View(vars_acs)
 vars_acs_profile<-load_variables(2020, "acs5/profile")
 View(vars_acs_profile)
 
-# get data for CA
 
-population<-get_acs(
-  geography="tract", 
-  survey="acs5", 
-  variable=c(population ="B01003_001"), 
-  key=api_key, 
-  state=c(06, 04),
-  geometry= TRUE)
+# Analysis ----------------------------------------------------------------
 
-pop_sacramento<-get_acs(
-  geography="tract", 
-  survey="acs5", 
-  variable=c(population ="B01003_001"), 
-  key=api_key, 
-  state="CA",
-  county = "Sacramento",
-  geometry= TRUE) #geometry parameter turns the table into an sf object
- 
-plot(pop_sacramento["estimate"])
-
-
-
-
+# List the FIPS codes for the states for which you want to get data
 all_fips<-c(1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56, 72) #72 is PR
 
-tract_data<-get_acs(
-  geography="tract", 
-  survey="acs5", 
+
+
+# Race Analysis -----------------------------------------------------------
+
+# variables to download
+race_vars = c(
+  all = "P2_001N", # All
+  hisp = "P2_002N", # Hispanic
+  white = "P2_005N", # White
+  baa = "P2_006N", # Black or African American
+  amin = "P2_007N", # American Indian
+  asian = "P2_008N", # Asian
+  nhopi = "P2_009N", # Native Hawaiian or Pacific Islander
+  other = "P2_010N", # Some Other Race
+  multi = "P2_011N" # Two or More Races
+)
+
+# get the data
+race <- get_decennial(
+  geography = 'tract',
+  variables = race_vars,
   year = 2020,
-  variable=c(population ="B01003_001"), 
-  key=api_key, 
-  state=all_fips,
-  geometry= TRUE)
+  geometry = F,
+  cache_table = TRUE,
+  key = api_key,
+  state = all_fips
+)
 
-saveRDS(tract_data, file="./data/tract_population.rds")
+# reshape the table into one column per variable
+race_wide<-cast(race, GEOID~variable, mean)
 
-plot(tract_data["estimate"], 
-     xlim=c(-125,-64.4),  
-     ylim=c(25,50),
-     breaks="quantile",
-     border=FALSE)
+# calculate the estimated number of people from each category in each partial census polygon
+
+#   Join isochrone data - by GEOID
+
+
+
+
+
+
+# # get data for CA
+# 
+# population<-get_acs(
+#   geography="tract", 
+#   survey="acs5", 
+#   variable=c(population ="B01003_001"), 
+#   key=api_key, 
+#   state=c(06, 04),
+#   geometry= TRUE)
+# 
+# pop_sacramento<-get_acs(
+#   geography="tract", 
+#   survey="acs5", 
+#   variable=c(population ="B01003_001"), 
+#   key=api_key, 
+#   state="CA",
+#   county = "Sacramento",
+#   geometry= TRUE) #geometry parameter turns the table into an sf object
+# 
+# plot(pop_sacramento["estimate"])
+# tract_data<-get_acs(
+#   geography="tract", 
+#   survey="acs5", 
+#   year = 2020,
+#   variable=c(population ="B01003_001"), 
+#   key=api_key, 
+#   state=all_fips,
+#   geometry= TRUE)
+# 
+# saveRDS(tract_data, file="./data/tract_population.rds")
+# 
+# plot(tract_data["estimate"], 
+#      xlim=c(-125,-64.4),  
+#      ylim=c(25,50),
+#      breaks="quantile",
+#      border=FALSE)
