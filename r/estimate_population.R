2ba51558cd23a6457795dc3537a5121eb0bd220f
alisonsnwong <asnwong@ucdavis.edu>
2023-01-31 16:41:14 -0800

    Update estimate_population.R

--- r/estimate_population.R
+++ r/estimate_population.R
@@ -6,7 +6,7 @@ library(stringr)
 library(tidycensus)
 library(tidyverse)
 
-census_api_key("API KEY", install = TRUE)
+census_api_key("api_key.txt", install = TRUE)
 load_variables(2020, "acs5", cache=TRUE)
 load_variables(2020, "pl")
 
@@ -73,7 +73,7 @@ read_census_variable_csv <- function(x) {
 setwd("~/git/graves-endocrine_surgeons")
 
 # Use function to pull population data from the tidycensus package
-csv <- "docs/census_variables.csv"
+csv <- "census_variables.csv"
 census_df <- read_census_variable_csv(csv) 
 
 # Read chosen census variable from csv to create vector for arranging the final table in order 
@@ -84,15 +84,16 @@ var_order <- names(var)
 
 ##### Isochrone section
 # List isochrone files from directory
-files <- list.files("data/gpkg/", pattern="*.gpkg", full.names=TRUE)
+files <- list.files("data/", pattern="*.gpkg", full.names=TRUE)
 
 # Format attribute table from each isochrone file to its own data frame within a large list 'ldf'
 ldf <- lapply(files, st_read) # read files to ldf 
 ldf <- lapply(ldf, st_drop_geometry) # Remove geometry to decrease run time
 
 # Filename saved to to name future data frames within a list 
-filenames <- list.files("data/gpkg/", pattern="*.gpkg", full.names=FALSE)
+filenames <- list.files("data/", pattern="*.gpkg", full.names=FALSE)
 
+##### Table joining
 # Left join census income table to tracts table where each isochrone (inside & ouside) is a list within the join_list
 join_list <- list()
 for (i in 1:length(ldf)){
