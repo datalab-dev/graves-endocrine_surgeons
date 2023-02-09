5e38662a1c00a5e8b45279e1e6eb51f0892f6907
Michele Tobias <mmtobias@ucdavis.edu>
2023-02-08 16:38:09 -0800

    load the data needed

---
+++ r/rurality_calculation.R
@@ -0,0 +1,39 @@
+#GOAL: how many people in each isochrone are considered rural?
+
+
+# Set Up ------------------------------------------------------------------
+
+# Libraries
+library(tidycensus)
+library(readxl)
+
+
+# API Access
+api_key<-readLines("census_API_key.txt")
+census_api_key(api_key)
+
+# Prepare Data -----------------------------------------------------------
+
+#read the FORHP rural designations table
+FORHP_rural_designation<-read_xlsx("./data/rural_areas/non-metro-counties-cts.xlsx")
+
+#if you want to see all the variables available from the census
+vars_2010<-load_variables(
+  year = 2010, 
+  "pl", 
+  cache = TRUE
+)
+
+# The FIPS codes for the states' data we want to download = all US + PR
+all_fips<-c(1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56, 72) #72 is PR
+
+# download the tract geometries with the population (because we need to pick some variable to get the geometries)
+# note that the TIGER Census site does not have the 2010 tract geometies to download for the whole county, so we're getting them from tidycensus.
+tract_data<-get_acs(
+  geography="tract", 
+  survey="acs5", 
+  year = 2010,
+  variable=c(population ="B01003_001"), 
+  key=api_key, 
+  state=all_fips,
+  geometry= TRUE)
